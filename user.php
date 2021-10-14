95b970a96cec80315982c210a12d5f822aa688a7
Darshini0402 <darshini.r0402@gmail.com>
2021-10-14 12:14:44 +0530

    user

--- user.php
+++ user.php
@@ -32,19 +32,20 @@
     <body>
         
         <h1 style="font-weight: bold; font-family: 'Times New Roman', Times, serif; color: black; font-size: 50px; text-align: center;">Hotel Management System</h1>
-        <form>
+        <form action="" method="POST">
         <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Username</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="inputname" placeholder="Username" style="width:200px;">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Password" style="width:200px;">
-    </div>
-  </div>
-</form>
+            <label for="inputPassword" class="col-sm-2 col-form-label">Username</label>
+            <div class="col-sm-10">
+            <input type="text" class="form-control" id="inputname" placeholder="Username" style="width:200px;">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10">
+            <input type="password" class="form-control" id="inputPassword" placeholder="Password" style="width:200px;">
+            </div>
+        </div>
+        <input type="submit" placeholder="Login">
+        </form>
     </body>
 </html>
