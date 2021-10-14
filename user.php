f38912c0a3e423222ea4335fecd03b99c808ff4d
Dikshita Mehta <83219841+dikshita-mehta@users.noreply.github.com>
2021-10-14 09:50:14 +0530

    Update user.php

--- user.php
+++ user.php
@@ -17,16 +17,16 @@
         
         <h1 style="font-weight: bold; font-family: 'Times New Roman', Times, serif; color: black; font-size: 50px; text-align: center;">Hotel Management System</h1>
         <form>
-  <div class="form-group row">
-    <label for="staticname" class="col-sm-2 col-form-label">Username</label>
+        <div class="form-group row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Username</label>
     <div class="col-sm-10">
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+      <input type="text" class="form-control" id="inputname" placeholder="Username" style="width:200px;">
     </div>
   </div>
   <div class="form-group row">
     <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+      <input type="password" class="form-control" id="inputPassword" placeholder="Password" style="width:200px;">
     </div>
   </div>
 </form>
